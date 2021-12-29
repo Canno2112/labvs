5b236bc42becab76105c6a2d29ce835adfcf1396
unknown <ahmedyoussry552gamil.com>
2021-12-29 18:40:14 +0200

    ahmed youssry edit

---
+++ cont_lab.js
@@ -0,0 +1,6 @@
+alert ('welcome in lab 1 team asmaa and 3 ahmed ');
+
+console.log ("hello world");
+console.log ("console from asmaa");
+console.log ("hi hi");
+console.log("hello from ahmed yousry");
